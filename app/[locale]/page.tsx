69ed33c6c010c68cbd98b3c4edc9fc36360d0c9d
Munkh Myangan <munkh-erdene.myangan@brighthr.com>
2024-08-22 17:16:31 +0100

    testing

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,13 @@
 import { ReactElement } from "react";
 
 export const dynamicParams = false;
+export const dynamic = "force-static";
 
 export async function generateStaticParams() {
   const locale = ["en", "ca", "ie"];
   return locale;
 }
 
-export default function BlogPostPage(props: any) {
-  return <p>test</p>;
+export default function BlogPostPage({ params }: { params: any }) {
+  return <p>test {params.locale}</p>;
 }
