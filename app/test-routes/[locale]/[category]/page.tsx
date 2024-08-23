17e31dbb15c491fc38cbe7f9a7200fe5e64d4fb7
Munkh Myangan <munkh-erdene.myangan@brighthr.com>
2024-08-23 13:44:02 +0100

    testing 2000 pages

--- app/test-routes/[locale]/[category]/page.tsx
+++ app/test-routes/[locale]/[category]/page.tsx
@@ -10,7 +10,7 @@ export const dynamicParams = false;
 export function generateStaticParams() {
   const arr = [];
   const locale = ["en", "ca", "ie"];
-  for (let i = 0; i < 1300; i += 1) {
+  for (let i = 0; i < 2000; i += 1) {
     for (let j = 0; j < locale.length; j += 1) {
       arr.push({ locale: locale[j], category: `page-${i}` });
     }
